08c73c703df53ee4391595644b55887199b51e4f
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-01-11 15:37:22 -0700

    task 7 completed

--- index.js
+++ index.js
@@ -264,8 +264,18 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(number){
+  if(number >= 90){
+    return 'you got an A';
+  } else if(number >= 80 && number <= 89){
+    return 'you got a B';
+  } else if(number>=70 && number <= 79){
+    return 'you got a C';
+  }else if(number >= 60 && number <= 69){
+    return 'you got a D';
+  }else{
+    return 'you got an F';
+  }
 }
 
 
